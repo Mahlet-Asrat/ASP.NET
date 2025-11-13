3d3868503fc4a83948ee2e7c567c1b1a7a063643
Mahlet Asrat <149795704+Mahlet-Asrat@users.noreply.github.com>
2025-11-13 23:11:47 +0300

    Fix syntax error

--- Models/School.cs
+++ Models/School.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public required string Name { get; set; }
         public string? Director { get; set; }
-        public List<string>? Teachers { get; set; };
+        public List<string>? Teachers { get; set; }
         public List<string>? Students { get; set; }
     }
 }
